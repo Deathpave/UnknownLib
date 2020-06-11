93fd8ff3705f5fc36dc7d1c5a46e997567c765d8
Martin Madsen <martin@endsearch.dk>
2020-06-11 13:06:05 +0200

    started classes

--- UnknownLib/UnknownLib/Files/ToTxt.cs
+++ UnknownLib/UnknownLib/Files/ToTxt.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UnknownLib
+namespace UnknownLib.Files
 {
-    public class Class1
+    class ToTxt
     {
     }
 }
