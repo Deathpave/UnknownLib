e481d9d00cce506cf178d4d03629332a7b35bb9c
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-09-11 12:23:18 +0200

    code comment

--- UnknownLib/UnknownLib/Objects/ReadObject.cs
+++ UnknownLib/UnknownLib/Objects/ReadObject.cs
@@ -11,14 +11,22 @@ namespace UnknownLib.Objects
     {
         public List<string> ObjectProperties(object o)
         {
+            // creates a new list for strings
             List<string> result = new List<string>();
+
+            // gets the type of the input object
             Type type = o.GetType();
+
+            // gets the properties of the object
             PropertyInfo[] props = type.GetProperties();
 
+            // goes through all properties and adds the to result list as strings
             foreach (PropertyInfo prop in props)
             {
                 result.Add(prop.GetValue(o, null).ToString());
             }
+
+            // returns the result list with object properties
             return result;
         }
     }
