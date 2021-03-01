fbf6cfd8de91fe833e06d03fc447ed471a551bc1
Martin Madsen <martin_riehn_madsen@hotmail.com>
2021-03-01 12:19:56 +0100

    started hashing

---
+++ UnknownLib/UnknownLib/Hashing/HashType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnknownLib.Hashing
+{
+    public enum HashType
+    {
+        Md5Hash,
+        Sha1Hash,
+        Sha2Hash,
+        Sha3Hash,
+        Sha5Hash,
+        HmacSha1,
+        HmacSha2
+    }
+}
--- UnknownLib/UnknownLib/Managers/SecurityManager.cs
+++ UnknownLib/UnknownLib/Managers/SecurityManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnknownLib.Decryption;
 using UnknownLib.Encryption;
+using UnknownLib.Hashing;
 using UnknownLib.Managers.Interfaces;
 
 namespace UnknownLib.Managers
@@ -32,6 +33,78 @@ namespace UnknownLib.Managers
         public string EncryptString(string input, string password, int itterations)
         {
             return encrypt.EncryptString(input, password, itterations);
+        }
+
+        // hashing overloads for different inputs
+        // string input, byte input, with and without password
+        public void Hash(HashType type, string input)
+        {
+            switch (type)
+            {
+                case HashType.Md5Hash:
+                    break;
+                case HashType.Sha1Hash:
+                    break;
+                case HashType.Sha2Hash:
+                    break;
+                case HashType.Sha3Hash:
+                    break;
+                case HashType.Sha5Hash:
+                    break;
+            }
+        }
+
+        public void Hash(HashType type, string input, string password)
+        {
+            switch (type)
+            {
+                case HashType.HmacSha1:
+                    break;
+                case HashType.HmacSha2:
+                    break;
+            }
+        }
+
+        public void Hash(HashType type, byte[] input)
+        {
+            switch (type)
+            {
+                case HashType.Md5Hash:
+                    break;
+                case HashType.Sha1Hash:
+                    break;
+                case HashType.Sha2Hash:
+                    break;
+                case HashType.Sha3Hash:
+                    break;
+                case HashType.Sha5Hash:
+                    break;
+                case HashType.HmacSha1:
+                    break;
+                case HashType.HmacSha2:
+                    break;
+            }
+        }
+
+        public void Hash(HashType type, byte[] input, byte[] password)
+        {
+            switch (type)
+            {
+                case HashType.Md5Hash:
+                    break;
+                case HashType.Sha1Hash:
+                    break;
+                case HashType.Sha2Hash:
+                    break;
+                case HashType.Sha3Hash:
+                    break;
+                case HashType.Sha5Hash:
+                    break;
+                case HashType.HmacSha1:
+                    break;
+                case HashType.HmacSha2:
+                    break;
+            }
         }
     }
 }
