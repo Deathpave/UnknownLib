dee655af1357a1caf8b7d7e5a6f885922393d645
Martin Riehn Madsen <martin_riehn_madsen@hotmail.com>
2021-04-07 12:02:33 +0200

    Created HookManager

---
+++ UnknownLib/UnknownLib/Managers/HookManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnknownLib.Hooks;
+
+namespace UnknownLib.Managers
+{
+    public class HookManager
+    {
+        private KeyboardHook _keyboardhook = new KeyboardHook();
+
+        public void StartKeyboardHook()
+        {
+            _keyboardhook.StartHook();
+        }
+
+        public void StopKeyboardHook()
+        {
+            _keyboardhook.StopHook();
+        }
+
+        public string GetKeyboardKey()
+        {
+            return _keyboardhook.Returnkey();
+        }
+    }
+}
