a016921bc6e7a3053783be2b88f69ae36c45a8f9
Martin Riehn Madsen <martin_riehn_madsen@hotmail.com>
2021-04-14 14:35:00 +0200

    Started recursive testing

---
+++ UnknownLib/UnknownLib/TestEnvironment/TestClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnknownLib.TestEnvironment
+{
+    public class TestClass
+    {
+        private List<string> _pathNames;
+
+        public TestClass()
+        {
+            _pathNames = new List<string>();
+        }
+
+        public void RecursiveMapping(string folderPath)
+        {
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(folderPath))
+                {
+                    _pathNames.Add(dir);
+                    try
+                    {
+                        foreach (string file in Directory.GetFiles(folderPath))
+                        {
+                            _pathNames.Add(file);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+                    RecursiveMapping(dir);
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
